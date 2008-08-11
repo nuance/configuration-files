309a108c1c383ecc67f72918ffcbd51a377820ec
Matt Jones <matt-biz@mhjones.org>
2008-08-10 20:06:45 -0700

    emacs config directory

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-$CONFIG_DIR=`pwd`
+CONFIG_DIR=`pwd`
 
 pushd ~
+
+# Link emacs directory (so the paths in the emacs config work)
+ln -s $CONFIG_DIR/emacs emacs
 
 # Set up configuration files
 ln -s $CONFIG_DIR/emacs/dotemacs .emacs
@@ -10,3 +13,5 @@ ln -s $CONFIG_DIR/dotfiles/dotzshrc .zshrc
 ln -s $CONFIG_DIR/dotfiles/dotscreenrc .screenrc
 
 popd
+
+echo "Configured!"
