f9221322cdff18dde6f51d4e5f94dc159f7fbf81
mattj <mattj@Matt-Joness-MacBook-Pro.local>
2008-08-13 23:47:04 -0700

    SSH keys / config

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ pushd ~
 # Link emacs directory (so the paths in the emacs config work)
 ln -s $CONFIG_DIR/emacs emacs
 
+# Link ssh directory
+ln -s $CONFIG_DIR/ssh .ssh
+
 # Set up configuration files
 ln -s $CONFIG_DIR/emacs/dotemacs .emacs
 ln -s $CONFIG_DIR/dotfiles/dotzshrc .zshrc
