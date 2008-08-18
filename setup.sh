91c69b8b54f6abc7cbfeb2f5376f428de9936371
Matt Jones <mattj@yelp.com>
2008-08-18 11:36:21 -0700

    Add some scripts, add shell includes directory

--- setup.sh
+++ setup.sh
@@ -15,6 +15,15 @@ ln -s $CONFIG_DIR/emacs/dotemacs .emacs
 ln -s $CONFIG_DIR/dotfiles/dotzshrc .zshrc
 ln -s $CONFIG_DIR/dotfiles/dotscreenrc .screenrc
 
+# Add scripts
+mkdir bin
+ln -s $CONFIG_DIR/bin/epylint bin/
+ln -s $CONFIG_DIR/bin/adhoc-sql bin/
+
+# Add shell includes
+mkdir includes
+ln -s $CONFIG_DIR/includes/svn-magic.sh includes/
+
 popd
 
 echo "Configured!"
