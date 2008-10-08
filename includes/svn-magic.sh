3bdde5b37bf6816bf153c5e4950f067c95cb9d3f
Matt <mattj@dev01.706.yelpcorp.com>
2008-10-08 16:44:07 -0700

    switch svn host names

--- includes/svn-magic.sh
+++ includes/svn-magic.sh
@@ -8,8 +8,8 @@ function newbranch {
 
     echo "Creating branch $branchname"
 
-    echo "*** svn copy https://svn/loc/main https://svn/loc/branch/$branchname ***"
-    svn copy https://svn/loc/main https://svn/loc/branch/$branchname
+    echo "*** svn copy svn+ssh://svn.yelpcorp.com/loc/main svn+ssh://svn.yelpcorp.com/loc/branch/$branchname ***"
+    svn copy svn+ssh://svn.yelpcorp.com/loc/main svn+ssh://svn.yelpcorp.com/loc/branch/$branchname
 
     cd ~/pg
     echo "*** copg branch/$branchname $name ***"
@@ -37,9 +37,9 @@ function switchtonewbranch {
 
     echo "Creating branch $branchname"
 
-    echo "*** svn copy https://svn/loc/main https://svn/loc/branch/$branchname ***"
-    svn copy https://svn/loc/main https://svn/loc/branch/$branchname
-	svn switch https://svn/loc/branch/$branchname
+    echo "*** svn copy svn+ssh://svn.yelpcorp.com/loc/main svn+ssh://svn.yelpcorp.com/loc/branch/$branchname ***"
+    svn copy svn+ssh://svn.yelpcorp.com/loc/main svn+ssh://svn.yelpcorp.com/loc/branch/$branchname
+	svn switch svn+ssh://svn.yelpcorp.com/loc/branch/$branchname
 
 	folder=`pwd`
 
@@ -79,7 +79,7 @@ function blog {
 }
 
 function bdiff {
-    svn diff -r $(bstart):$(bstop) https://svn/loc/branch/$(bname) | less
+    svn diff -r $(bstart):$(bstop) svn+ssh://svn.yelpcorp.com/loc/branch/$(bname) | less
 }
 
 # Convenience functions
@@ -87,12 +87,12 @@ function bdiff {
 function bmerge {
     echo "Branch name: $(bname)"
     echo "Revision range: $(bstart):$(bstop)"
-    echo "Merge string: merge -r $(bstart):$(bstop) https://svn/loc/branch/$(bname)"
+    echo "Merge string: merge -r $(bstart):$(bstop) svn+ssh://svn.yelpcorp.com/loc/branch/$(bname)"
 }
 
 function lsbranch {
     # Assuming you're using my naming convention, there is a Y3K bug here
-    svn ls https://svn/loc/branch/ | egrep "^${USER}\_2"
+    svn ls svn+ssh://svn.yelpcorp.com/loc/branch/ | egrep "^${USER}\_2"
 }
 
 function switchloc {
