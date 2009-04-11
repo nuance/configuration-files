6801075a35fa017470a96e6da818f7f4a2a50579
Matt Jones <matt-biz@mhjones.org>
2009-04-11 14:57:25 -0700

    remove refs to ssh

--- setup.sh
+++ setup.sh
@@ -7,13 +7,6 @@ pushd ~
 # Link emacs directory (so the paths in the emacs config work)
 ln -s $CONFIG_DIR/emacs emacs
 
-# Link ssh directory if it doesn't exist, otherwise link individual files
-if [[ -e .ssh ]]; then
-  ln -s $CONFIG_DIR/ssh/* .ssh/
-else
-  ln -s $CONFIG_DIR/ssh .ssh
-fi
-
 # Set up configuration files
 ln -s $CONFIG_DIR/emacs/dotemacs .emacs
 ln -s $CONFIG_DIR/dotfiles/dotzshrc .zshrc
@@ -22,7 +15,6 @@ ln -s $CONFIG_DIR/dotfiles/dotscreenrc .screenrc
 # Add scripts
 mkdir bin
 ln -s $CONFIG_DIR/bin/epylint bin/
-ln -s $CONFIG_DIR/bin/adhoc-sql bin/
 
 # Add shell includes
 mkdir includes
