4c77d49eebf9c57503f867132539b65209ff9342
Matt Jones <mattj@yuance.(none)>
2009-05-26 10:35:35 -0700

    more fixin

---
+++ emacs/yuance.el
@@ -0,0 +1,8 @@
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "unknown" :family "Consolas"))))
+ '(highline-face ((t (:background "gray15")))))
+
