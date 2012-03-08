b149339ee6b2e1b36116dcbd4fa42fba2ba764f9
Matt Jones <mattj@yelp.com>
2012-03-08 11:22:19 -0800

    java customizations

--- emacs/programming/nuance-java.el
+++ emacs/programming/nuance-java.el
@@ -1,5 +1,28 @@
+ ;; Or enable more if you wish
+(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
+								  global-semanticdb-minor-mode
+								  global-semantic-idle-summary-mode
+								  global-semantic-mru-bookmark-mode))
+(semantic-mode 1)
+
+;; (require 'malabar-mode)
+
+;; (setq malabar-groovy-lib-dir (concat dotfiles-dir "/elpa-to-submit/malabar-mode-1.4.0/lib"))
+;; (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+
+(require 'eclim)
+
+(setq eclim-auto-save t)
+(global-eclim-mode)
+
+(require 'company-emacs-eclim)
+(company-emacs-eclim-setup)
+
 (require 'ac-company)
-(ac-company-define-source ac-source-company-elisp company-elisp)
+(ac-company-define-source ac-source-company-eclim company-emacs-eclim)
 (add-hook 'java-mode-hook
 		  (lambda () 
 			(add-to-list 'ac-sources 'ac-source-company-eclim)))
+
+(provide 'nuance-java)
+
